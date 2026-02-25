242ea9aef943f7564980362f23954db6f337aec9
Rak-att <codelabnyc@gmail.com>
2026-02-25 09:49:33 -0800

    Insert default sectors

--- api/src/defaults/__init__.py
+++ api/src/defaults/__init__.py
@@ -8,6 +8,7 @@ from src.defaults.files import insert_default_files_for_tenant
 from src.defaults.countries import insert_default_countries_for_tenant
 from src.defaults.currencies import insert_default_currencies_for_tenant
 from src.defaults.market_categories import insert_default_market_categories_for_tenant
+from src.defaults.sectors import insert_default_sectors_for_tenant
 from src.defaults.security_subtypes import insert_default_security_subtypes_for_tenant
 from src.defaults.column_definitions import insert_default_column_definitions_for_tenant
 from src.defaults.security_types import insert_default_security_types_for_tenant
@@ -24,6 +25,7 @@ async def insert_defaults_for_tenant(session: AsyncSession, tenant_id: uuid.UUID
     await insert_default_countries_for_tenant(session, tenant_id)
     await insert_default_currencies_for_tenant(session, tenant_id)
     await insert_default_market_categories_for_tenant(session, tenant_id)
+    await insert_default_sectors_for_tenant(session, tenant_id)
     await insert_default_security_types_for_tenant(session, tenant_id)
     await insert_default_security_subtypes_for_tenant(session, tenant_id)
     await insert_default_files_for_tenant(session, tenant_id)
